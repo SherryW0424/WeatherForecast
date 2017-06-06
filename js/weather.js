803632d415ef8459d78b808608130f0f476db7a2
SherryW0424 <sherryw0424@gmail.com>
2017-06-06 23:13:31 +0800

    地图下钻完成~

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,11 @@
 function checkWeather() {
     document.getElementById('divbutton2').innerHTML=jsonObject.HeWeather5[0].basic.city;
 }
+
+function loadScript(url) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = url;
+    document.body.appendChild(script);
+    console.log(url);
+}
